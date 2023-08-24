b00da43b89d73b028cad38a07761e344376f4638
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-24 09:32:45 +0300

    Initial Commit

---
+++ birthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birthdayApp
+//
+//  Created by Fatih Oğuz on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
