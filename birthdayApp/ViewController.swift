264c43a8e1011a793d9acb9fd99782e92ca1047b
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-25 19:00:44 +0300

    duzenlemeler yapildi

--- birthdayApp/ViewController.swift
+++ birthdayApp/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NameTextFiled: UITextField!
+    @IBOutlet weak var surnameTextFiled: UITextField!
+    @IBOutlet weak var birthdayTextFiled: UITextField!
+    
+    @IBOutlet weak var NameText: UILabel!
+    @IBOutlet weak var surnameText: UILabel!
+    @IBOutlet weak var birthdayText: UILabel!
+    
     override func viewDidLoad() {
+       
+        
+       
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedSurname = UserDefaults.standard.object(forKey: "surname")
+        
+         let storedBirthday =  UserDefaults.standard.object(forKey: "birthday")
+        if let newName = storedName as? String {
+            
+            NameText.text = "Name: \(newName)"
+        }
+        if let newSurname = storedSurname as? String{
+            surnameText.text = "Surname: \(newSurname)"
+        }
+        
+        if let newBirthday = storedBirthday as? String{
+            birthdayText.text = "Birthday: \(newBirthday)"
+        }
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func saveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(NameTextFiled.text, forKey: "name")
+        UserDefaults.standard.set(surnameTextFiled.text, forKey: "surname")
+        UserDefaults.standard.set(birthdayTextFiled.text, forKey: "birthday")
+        
+        NameText.text = "Name: \(NameTextFiled.text!)"
+        surnameText.text = "Surname: \(surnameTextFiled.text!)"
+        birthdayText.text = "Birthday: \(birthdayTextFiled.text!)"
 
+    }
+    
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedSurname = UserDefaults.standard.object(forKey: "surname")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if ((storedName as? String ) != nil) {
+            UserDefaults.standard.removeObject(forKey: "name")
+            NameText.text = "Name: "
+        }
+        if ((storedSurname as? String) != nil){
+            UserDefaults.standard.removeObject(forKey: "surname")
+            surnameText.text = "Surname: "
+        }
+        if ((storedBirthday as? String) != nil){
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayText.text = "Birthday: "
+        }
+    }
+    
+    
 }
-
